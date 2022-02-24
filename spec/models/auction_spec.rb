1fa9bd9caa525ddb3c76177ae6375c440ab4417f
Lukas Musilek <lukas.musilek@gmail.com>
2022-02-24 06:16:29 +0000

    rspec for Auction model done

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Auction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:auction_subject) { Auction.new(title: "Kukri", description: "Cool dagger", start_date: DateTime.now, end_date: DateTime.now + 1.week) }
+
+  it "is valid with valid attributes" do
+    auction = auction_subject
+    
+    expect(auction).to be_valid
+  end
+
+  it "is not valid without a title" do
+    auction = auction_subject
+
+    auction.title = nil
+    
+    expect(auction).to_not be_valid
+  end
+
+  it "is not valid without a description" do
+    auction = auction_subject
+
+    auction.description = nil
+
+    expect(auction).to_not be_valid
+  end
+
+  it "is not valid without a start_date" do
+    auction = auction_subject
+
+    auction.start_date = nil
+    
+    expect(auction).to_not be_valid
+  end
+
+  it "is not valid without a end_date" do 
+    auction = auction_subject
+
+    auction.end_date = nil
+      
+    expect(auction).to_not be_valid
+  end
 end
