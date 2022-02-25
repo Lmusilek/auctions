1a7ab62ea29fd0ffd963537041b7ee352f5d4958
Lukas Musilek <lukas.musilek@gmail.com>
2022-02-25 08:15:00 +0000

    refactored variables in spec file

--- spec/models/bidding_engine_spec.rb
+++ spec/models/bidding_engine_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe BiddingEngine, type: :model do
-
+  
+  let(:seller) { User.create(:email => 'jane@doe.com', :password => 'pw1234') }
+  let(:bidder) { User.create(:email => 'john@doe.com', :password => 'pw1234') }
+  let(:auction) { Auction.create(title: 'Anything', description: 'Lorem ipsum', start_date: DateTime.now, end_date: DateTime.now + 1.week, user_id: seller.id) }
 
   describe ".bid!" do
     it "create a new bid on an auction" do
-      seller = User.create(:email => 'jane@doe.com', :password => 'pw1234' )
-      bidder = User.create(:email => 'john@doe.com', :password => 'pw1234' )
-
-      auction = Auction.create(title: 'Anything', description: 'Lorem ipsum',
-                   start_date: DateTime.now, end_date: DateTime.now + 1.week,
-                   user_id: seller.id)
-
+      
       BiddingEngine.bid!(100, bidder, auction)
       expect(auction.errors).to be_empty
 
